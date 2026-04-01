626246b72609cbfa06c655a1e1d3430b8c6e6f15
Jiayi-Wang-Joey <591813079@qq.com>
2026-04-01 17:14:01 +0200

    add run.sh

--- run.sh
+++ run.sh
@@ -14,155 +14,6 @@ for arg in "$@"; do
     prev="$arg"
 done
 
-dispatch_collector() {
-    for arg in "$@"; do
-        case "$arg" in
-            --variant.vcf|--variant_vcf|--variant-vcf)
-                exec Rscript "$DIR/plot_upset.R" "$@"
-                ;;
-        esac
-    done
-}
-
-dispatch_legacy() {
-    for arg in "$@"; do
-        case "$arg" in
-            --s3_url)
-                exec "$DIR/download.sh" "$@"
-                ;;
-            --reference_genome)
-                exec "$DIR/align.sh" "$@"
-                ;;
-        esac
-    done
-}
-
-dispatch_from_parameters_json() {
-    STDERR_PATH="$(readlink -f /proc/self/fd/2 2>/dev/null || true)"
-    [ -n "$STDERR_PATH" ] || return 1
-
-    OUTDIR="$(dirname "$STDERR_PATH")"
-    PARAM_JSON="$OUTDIR/parameters.json"
-    [ -f "$PARAM_JSON" ] || return 1
-
-    # extract task if present
-    JSON_TASK="$(python - <<'PY' "$PARAM_JSON"
-import json, sys
-
-def find_key(obj, key):
-    if isinstance(obj, dict):
-        if key in obj and isinstance(obj[key], str):
-            return obj[key]
-        for v in obj.values():
-            out = find_key(v, key)
-            if out:
-                return out
-    elif isinstance(obj, list):
-        for v in obj:
-            out = find_key(v, key)
-            if out:
-                return out
-    return ""
-
-with open(sys.argv[1]) as fh:
-    data = json.load(fh)
-
-print(find_key(data, "task"))
-PY
-)"
-
-    if [ -n "$JSON_TASK" ]; then
-        case "$JSON_TASK" in
-            clair3_rna)
-                exec "$DIR/clair3_rna.sh" --task "$JSON_TASK"
-                ;;
-            deep_variant)
-                exec "$DIR/deep_variant.sh" --task "$JSON_TASK"
-                ;;
-            longcallR)
-                exec "$DIR/longcallR.sh" --task "$JSON_TASK"
-                ;;
-            longcallR_nn)
-                exec "$DIR/longcallR_nn.sh" --task "$JSON_TASK"
-                ;;
-            alignment_qc)
-                exec "$DIR/alignment_qc.sh" --task "$JSON_TASK"
-                ;;
-            gnomad_detection)
-                exec "$DIR/gnomad_detection.sh" --task "$JSON_TASK"
-                ;;
-            somatic_detection)
-                exec "$DIR/somatic_detection.sh" --task "$JSON_TASK"
-                ;;
-            align)
-                exec "$DIR/align.sh" --task "$JSON_TASK"
-                ;;
-        esac
-    fi
-
-    # rawdata fallback: detect s3_url in parameters.json
-    JSON_S3_URL="$(python - <<'PY' "$PARAM_JSON"
-import json, sys
-
-def find_key(obj, key):
-    if isinstance(obj, dict):
-        if key in obj and isinstance(obj[key], str):
-            return obj[key]
-        for v in obj.values():
-            out = find_key(v, key)
-            if out:
-                return out
-    elif isinstance(obj, list):
-        for v in obj:
-            out = find_key(v, key)
-            if out:
-                return out
-    return ""
-
-with open(sys.argv[1]) as fh:
-    data = json.load(fh)
-
-print(find_key(data, "s3_url"))
-PY
-)"
-
-    if [ -n "$JSON_S3_URL" ]; then
-        exec "$DIR/download.sh" --s3_url "$JSON_S3_URL"
-    fi
-
-    # align fallback if reference genome present
-    JSON_REF="$(python - <<'PY' "$PARAM_JSON"
-import json, sys
-
-def find_key(obj, key):
-    if isinstance(obj, dict):
-        if key in obj and isinstance(obj[key], str):
-            return obj[key]
-        for v in obj.values():
-            out = find_key(v, key)
-            if out:
-                return out
-    elif isinstance(obj, list):
-        for v in obj:
-            out = find_key(v, key)
-            if out:
-                return out
-    return ""
-
-with open(sys.argv[1]) as fh:
-    data = json.load(fh)
-
-print(find_key(data, "reference_genome"))
-PY
-)"
-
-    if [ -n "$JSON_REF" ]; then
-        exec "$DIR/align.sh" --reference_genome "$JSON_REF"
-    fi
-
-    return 1
-}
-
 case "$TASK" in
     clair3_rna)
         exec "$DIR/clair3_rna.sh" "$@"
@@ -192,9 +43,34 @@ case "$TASK" in
         exec "$DIR/align.sh" "$@"
         ;;
     "")
-        dispatch_collector "$@"
-        dispatch_legacy "$@"
-        dispatch_from_parameters_json
+        # collector autodetection
+        for arg in "$@"; do
+            case "$arg" in
+                --variant.vcf|--variant_vcf|--variant-vcf)
+                    exec Rscript "$DIR/plot_upset.R" "$@"
+                    ;;
+            esac
+        done
+
+        # old-style CLI autodetection
+        for arg in "$@"; do
+            case "$arg" in
+                --s3_url)
+                    exec "$DIR/download.sh" "$@"
+                    ;;
+                --reference_genome)
+                    exec "$DIR/align.sh" "$@"
+                    ;;
+            esac
+        done
+
+        # new fallback: Omnibenchmark rawdata jobs may pass no args at all
+        STDERR_PATH="$(readlink -f /proc/self/fd/2 2>/dev/null || true)"
+        case "$STDERR_PATH" in
+            *"/out/rawdata/"*)
+                exec "$DIR/download.sh"
+                ;;
+        esac
         ;;
     *)
         echo "ERROR: unknown task: $TASK" >&2
